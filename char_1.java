5539348f6283eaa2489575fc639a872562fbb581
Gautam Somani <gautamsomani@gmail.com>
2016-10-07 04:11:49 +0530

    char

---
+++ char_1.java
@@ -0,0 +1,15 @@
+class	char_1	{
+	public static void main(String arg[])	{
+		
+		char a, b;
+
+		a = 88;
+		b = 'Y';
+		System.out.println("a and b are : " + a + " " + b);
+
+		a = 'X';
+		b = 89;
+		System.out.println("a and b are : " + a + " " + b);
+
+	}
+}
