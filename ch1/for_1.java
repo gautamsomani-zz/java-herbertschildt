1726b1d6f91ce9a0fb927780f9628c40d865383b
Gautam Somani <gautamsomani@gmail.com>
2016-10-07 03:51:33 +0530

    for loop

---
+++ ch1/for_1.java
@@ -0,0 +1,8 @@
+class for_1	{
+	public static void main(String args[])	{
+		for (double number = 1 ; number < 20 ; number++)	{
+			double result = Math.pow(number,number);
+			System.out.println(result);
+		}
+	}
+}
