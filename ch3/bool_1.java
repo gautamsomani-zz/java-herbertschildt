c3ea0e4ef073f78878edd9453ae83c28f41bd62c
Gautam Somani <gautamsomani@gmail.com>
2016-10-07 04:46:02 +0530

    Boolean

---
+++ ch3/bool_1.java
@@ -0,0 +1,15 @@
+class	bool_1	{
+	public static void main(String args[])	{
+		
+		boolean a;
+
+		a = false;
+		System.out.println("a is " + a);
+
+		a = true;
+		System.out.println("a is " + a);
+
+		if(a)	System.out.println("a is true");
+
+	}
+}
