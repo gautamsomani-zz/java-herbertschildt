4d24ef228d55ff3f03e66a5ab663956ef8a7e804
Gautam Somani <gautamsomani@gmail.com>
2016-10-07 04:07:04 +0530

    ch3 float

---
+++ ch3/circle_area.java
@@ -0,0 +1,12 @@
+class	circle_area	{
+	public static void main(String args[])	{
+
+		int radius = 10;
+		double pi, area;
+
+		pi = 3.1416;
+		area = pi * radius * radius;
+
+		System.out.println("Area : " + area);
+	}
+}		
