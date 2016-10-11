1c6ac308b851aa24f48165133304c7457c2c3117
Gautam Somani <gautamsomani@gmail.com>
2016-10-11 19:31:53 +0530

    Array 1

---
+++ ch3/array_1.java
@@ -0,0 +1,13 @@
+class array_1	{
+	public static void main(String args[])	{
+		
+		int array1;
+		array1 = new int[4];
+
+		array1[0] = 10;
+		array1[1] = 20;
+
+		System.out.println("1st element in array1 is " + array1[0]);
+
+	}
+}
