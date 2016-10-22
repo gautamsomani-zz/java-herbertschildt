9d879992a945b180c16e7dc1657624f1b2f84a70
Gautam Somani <gautamsomani@gmail.com>
2016-10-22 19:53:09 +0530

    Going on

---
+++ ch5/contructor01.java
@@ -0,0 +1,37 @@
+class contructor01	{
+	public static void main(String args[])	{
+
+		System.out.println("Hello.");
+
+//		# new will give memory. mybox is a reference pointer to memory. new will instantiate.
+		Box mybox = new Box();
+		Box b2 = new Box();
+
+		mybox.lenght = 10;
+		mybox.breadth = 11;
+		mybox.height = 12;
+
+		b2.setData(50, 60, 70);
+
+		mybox.display();
+		b2.display();
+	}
+}
+
+class Box	{
+
+	double	lenght, breadth, height;
+	
+	void display()	{
+		System.out.println("mybox.lenght is " + lenght);
+		System.out.println("mybox.breadth is " + breadth);
+		System.out.println("mybox.height is " + height);
+	}
+
+	void setData(double a, double b, double c)	{
+		lenght = a;
+		breadth = b;
+		height = c;
+	} 
+
+}
